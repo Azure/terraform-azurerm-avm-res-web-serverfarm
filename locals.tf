0dd9be8b44712ad1e300354f5867140f0492f79e
Isabelle Bersano <ibersano@microsoft.com>
2024-04-16 11:51:33 -0400

    fix: if zones set to true, set worker_count to the number of zones in that region and removed duplicate local block

--- locals.tf
+++ locals.tf
@@ -3,8 +3,3 @@ locals {
   resource_group_location            = try(data.azurerm_resource_group.parent[0].location, null)
   role_definition_resource_substring = "/providers/Microsoft.Authorization/roleDefinitions"
 }
-
-# Private endpoint application security group associations
-# Remove if this resource does not support private endpoints
-locals {
-}
--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ data "azurerm_resource_group" "parent" {
   name = var.resource_group_name
 }
 
+data "azurerm_location" "region" {
+  location = coalesce(var.location, local.resource_group_location)
+}
+
 resource "azurerm_service_plan" "this" {
   location                     = coalesce(var.location, local.resource_group_location)
   name                         = var.name # calling code must supply the name
@@ -14,7 +18,7 @@ resource "azurerm_service_plan" "this" {
   maximum_elastic_worker_count = local.maximum_elastic_worker_count
   per_site_scaling_enabled     = var.per_site_scaling_enabled
   tags                         = var.tags
-  worker_count                 = var.worker_count
+  worker_count                 = var.zone_balancing_enabled? length(data.azurerm_location.region.zone_mappings): var.worker_count
   zone_balancing_enabled       = var.zone_balancing_enabled
 }
 
