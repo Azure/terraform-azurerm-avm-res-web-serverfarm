4cb7d643c19f623d09cd82b89b2f363b28325aba
Donovan McCoy <144926888+donovm4@users.noreply.github.com>
2024-11-21 16:46:43 -0500

    fix: support for `azurerm v4.x`, `FC1` `sku_name`, updates to regex validation for `name` (#70)
    * modifications to variables, fixing avm scripts, aligning default example
    
    * more modifications
    
    * removing zone mapping from output
    
    * removing commented sections, new logic for worker_count
    
    * initial commit
    
    * updating regex validation

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ data "azurerm_location" "region" {
 
 resource "azurerm_service_plan" "this" {
   location                     = var.location
-  name                         = var.name # calling code must supply the name
+  name                         = var.name
   os_type                      = var.os_type
   resource_group_name          = var.resource_group_name
   sku_name                     = var.sku_name
